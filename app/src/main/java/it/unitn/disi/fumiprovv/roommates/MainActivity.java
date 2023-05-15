ef7120ebbee6542c293a2b71cde69d1b10b58b4e
Lorenzo <lorenzofumi@gmail.com>
2023-05-15 18:51:17 +0200

    Added navigation, added login, registration and house creation fragments.

--- app/src/main/java/it/unitn/disi/fumiprovv/roommates/MainActivity.java
+++ app/src/main/java/it/unitn/disi/fumiprovv/roommates/MainActivity.java
@@ -1,6 +1,10 @@
 package it.unitn.disi.fumiprovv.roommates;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -10,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/login/LoginFragment.java
@@ -0,0 +1,62 @@
+package it.unitn.disi.fumiprovv.roommates.fragments.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import it.unitn.disi.fumiprovv.roommates.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment LoginFragment.
+     */
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_login, container, false);
+        Button loginBtn = (Button) view.findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener((a) -> onLoginButtonClick(view));
+        Button registrationBtn = (Button) view.findViewById(R.id.registrationButton);
+        registrationBtn.setOnClickListener((a) -> onRegistrationButtonClick(view));
+        return view;
+    }
+
+    public void onLoginButtonClick(View view) {
+        NavController navController = Navigation.findNavController(view);
+        navController.navigate(R.id.homeFragment);
+    }
+
+    public void onRegistrationButtonClick(View view) {
+        NavController navController = Navigation.findNavController(view);
+        navController.navigate(R.id.registrationFragment);
+    }
+}
