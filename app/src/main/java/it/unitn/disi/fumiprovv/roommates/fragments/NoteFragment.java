f62ab69a57493bfe6d57e2e00fb9d77bae13bed8
Lorenzo <lorenzofumi@gmail.com>
2023-05-24 12:03:34 +0200

    Added remove (trash can) icon. Added Note fragment.

---
+++ app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/NoteFragment.java
@@ -0,0 +1,92 @@
+package it.unitn.disi.fumiprovv.roommates.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import it.unitn.disi.fumiprovv.roommates.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NoteFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NoteFragment extends Fragment {
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public NoteFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NoteFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NoteFragment newInstance(String param1, String param2) {
+        NoteFragment fragment = new NoteFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_note, container, false);
+
+        ListView notesListView = view.findViewById(R.id.notesListView);
+
+
+        db.collection("notes").whereEqualTo("houseId", )
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        notesListView.setAdapter(adapter);
+
+        // Imposta il listener di click sugli elementi della lista
+        notesListView.setOnItemClickListener((AdapterView.OnItemClickListener) (parent, view1, position, id) -> {
+            String selectedItem = items[position];
+            // Esegui le azioni desiderate con l'elemento selezionato
+            // ...
+            dialog.dismiss(); // Chiudi il dialog dopo il click
+        });
+
+        return view;
+    }
+}
