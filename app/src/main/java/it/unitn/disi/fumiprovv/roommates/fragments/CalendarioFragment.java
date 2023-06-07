1e758921bba05a5ea9651f225f241a8790792cea
Lorenzo <lorenzofumi@gmail.com>
2023-06-07 21:58:22 +0200

    Fixed calendar: going back after adding new event; reloading the listview after adding new event; loading events at the start

--- app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/CalendarioFragment.java
+++ app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/CalendarioFragment.java
@@ -1,42 +1,30 @@
 package it.unitn.disi.fumiprovv.roommates.fragments;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Collections;
-import java.util.GregorianCalendar;
+import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.TimeZone;
 
 import it.unitn.disi.fumiprovv.roommates.R;
 import it.unitn.disi.fumiprovv.roommates.adapters.EventAdapter;
@@ -50,13 +38,12 @@ import it.unitn.disi.fumiprovv.roommates.viewmodels.HouseViewModel;
  */
 public class CalendarioFragment extends Fragment {
 
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -101,7 +88,7 @@ public class CalendarioFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_calendario, container, false);
 
         CalendarView calendarView = view.findViewById(R.id.calendar_view);
-        Button b = view.findViewById(R.id.button_aggiungi_evento);
+        Button newEventButton = view.findViewById(R.id.button_aggiungi_evento);
         //TextView selectedDateTextView = view.findViewById(R.id.selected_date_text_view);
 
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
@@ -168,61 +155,29 @@ public class CalendarioFragment extends Fragment {
 
             }
         });*/
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date(calendarView.getDate()));
+        List<Event> filteredEvents = new ArrayList<>();
+        DocumentReference casa = db.collection("case").document(houseViewModel.getHouseId());
+        updateCalendar(casa, cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH),
+                cal.get(Calendar.YEAR), eventAdapter, filteredEvents);
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                List<Event> filteredEvents = new ArrayList<>();
-                filteredEvents.clear();
 
-                DocumentReference casa = db.collection("case").document(houseViewModel.getHouseId());
-                Log.d("house id", casa.toString());
+        calendarView.setOnDateChangeListener((view12, year, month, dayOfMonth) -> {
+            filteredEvents.clear();
+            Log.d("house id", casa.toString());
 
-                db.collection("eventi")
-                        .whereEqualTo("casa", casa)
-                        .whereEqualTo("giorno", dayOfMonth)
-                        .whereEqualTo("mese", month+1)
-                        .whereEqualTo("anno", year)
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot document : task.getResult()) {
+            updateCalendar(casa, dayOfMonth, month, year, eventAdapter, filteredEvents);
 
-                                        Map<String, Object> data = document.getData();
-
-                                        DocumentReference casa = (DocumentReference) data.get("casa");
-                                        Long giorno = (Long) data.get("giorno");
-                                        Long mese = (Long) data.get("mese");
-                                        Long anno = (Long) data.get("anno");
-                                        String nome = (String) data.get("nome");
-                                        Event temp = new Event(casa, nome, giorno, mese, anno);
-
-                                        filteredEvents.add(temp);
-                                        Log.d("prova", temp.toString());
-                                    }
-                                    eventAdapter.setData(filteredEvents);
-                                } else {
-                                    Log.d("error", "Error getting documents: ", task.getException());
-                                }
-                            }
-                        });
-
-                Log.d("lista", filteredEvents.toString());
-                if (filteredEvents.isEmpty()) {
-                    // Clear the adapter's data
-                    eventAdapter.setData(Collections.emptyList());
-                }
+            Log.d("lista", filteredEvents.toString());
+            if (filteredEvents.isEmpty()) {
+                // Clear the adapter's data
+                eventAdapter.setData(Collections.emptyList());
             }
         });
 
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_calendarioFragment_to_nuovoEvento);
-            }
-        });
+        newEventButton.setOnClickListener(view1 ->
+                Navigation.findNavController(view1).navigate(R.id.action_calendarioFragment_to_nuovoEvento));
 
         /*db.collection("eventi")
                 .get()
@@ -248,6 +203,37 @@ public class CalendarioFragment extends Fragment {
                 });*/
 
         return view;
+    }
+
+    private void updateCalendar(DocumentReference casa, int dayOfMonth, int month, int year,
+                                EventAdapter eventAdapter, List<Event> filteredEvents) {
+        db.collection("eventi")
+                .whereEqualTo("casa", casa)
+                .whereEqualTo("giorno", dayOfMonth)
+                .whereEqualTo("mese", month + 1)
+                .whereEqualTo("anno", year)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+
+                            Map<String, Object> data = document.getData();
+
+                            DocumentReference casa1 = (DocumentReference) data.get("casa");
+                            Long giorno = (Long) data.get("giorno");
+                            Long mese = (Long) data.get("mese");
+                            Long anno = (Long) data.get("anno");
+                            String nome = (String) data.get("nome");
+                            Event temp = new Event(casa1, nome, giorno, mese, anno);
+
+                            filteredEvents.add(temp);
+                            Log.d("prova", temp.toString());
+                        }
+                        eventAdapter.setData(filteredEvents);
+                    } else {
+                        Log.d("error", "Error getting documents: ", task.getException());
+                    }
+                });
     }
 
     private void updateData(int year, int month, int dayOfMonth) {
--- app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/NuovoEvento.java
+++ app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/NuovoEvento.java
@@ -1,10 +1,6 @@
 package it.unitn.disi.fumiprovv.roommates.fragments;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,24 +8,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.Timestamp;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.sql.Time;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 import it.unitn.disi.fumiprovv.roommates.R;
 import it.unitn.disi.fumiprovv.roommates.models.Event;
-import it.unitn.disi.fumiprovv.roommates.utils.NavigationUtils;
+import it.unitn.disi.fumiprovv.roommates.viewmodels.HouseViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -42,13 +35,11 @@ public class NuovoEvento extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     public NuovoEvento() {
         // Required empty public constructor
@@ -84,45 +75,35 @@ public class NuovoEvento extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_nuovo_evento, container, false);
+        View view = inflater.inflate(R.layout.fragment_nuovo_evento, container, false);
         Button submitButton = view.findViewById(R.id.submitButton);
-        EditText nomeEvento = (EditText) view.findViewById(R.id.nomeEvento);
-        DatePicker dataEvento = (DatePicker) view.findViewById(R.id.dataEvento);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Retrieve input values
-                String eventName = nomeEvento.getText().toString();
-                int y = dataEvento.getYear();
-                int m = dataEvento.getMonth();
-                int d = dataEvento.getDayOfMonth();
-                Calendar c = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
-                c.set(y,m,d);
+        EditText nomeEvento = view.findViewById(R.id.nomeEvento);
+        DatePicker dataEvento = view.findViewById(R.id.dataEvento);
+        HouseViewModel houseViewModel = new ViewModelProvider(requireActivity()).get(HouseViewModel.class);
+        submitButton.setOnClickListener(v -> {
+            // Retrieve input values
+            String eventName = nomeEvento.getText().toString();
+            int y = dataEvento.getYear();
+            int m = dataEvento.getMonth();
+            int d = dataEvento.getDayOfMonth();
+            Calendar c = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
+            c.set(y, m, d);
 
-                DocumentReference casa = db.collection("case").document("OKBVOT");
+            DocumentReference casa = db.collection("case").document(houseViewModel.getHouseId());
 
-                // Create a data model object
-                Event event = new Event(casa, eventName, new Long(d), new Long(m+1), new Long(y));
+            // Create a data model object
+            Event event = new Event(casa, eventName, (long) d, (long) (m + 1), (long) y);
 
-                //aggiungi evento al db
-                db.collection("eventi")
-                        .add(event)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d("nuovoEvento", "DocumentSnapshot written with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w("nuovoEvento", "Error adding document", e);
-                            }
-                        });
-            }
+            //aggiungi evento al db
+            db.collection("eventi").add(event).addOnSuccessListener(documentReference -> {
+                Log.d("nuovoEvento", "DocumentSnapshot written with ID: " + documentReference.getId());
+                requireActivity().onBackPressed();
+            }).addOnFailureListener(e -> {
+                Log.w("nuovoEvento", "Error adding document", e);
+                requireActivity().onBackPressed();
+            });
         });
         return view;
     }
