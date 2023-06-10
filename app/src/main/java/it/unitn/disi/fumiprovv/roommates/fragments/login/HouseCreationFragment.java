ae7817e1642c510ba6b1d59dd6a33b519e25f0a9
Lorenzo <lorenzofumi@gmail.com>
2023-06-10 17:37:29 +0200

    Using DB transaction (batch) to join/create a house.

--- app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/login/HouseCreationFragment.java
+++ app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/login/HouseCreationFragment.java
@@ -3,10 +3,6 @@ package it.unitn.disi.fumiprovv.roommates.fragments.login;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,10 +12,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.WriteBatch;
 
 import it.unitn.disi.fumiprovv.roommates.MainActivity;
 import it.unitn.disi.fumiprovv.roommates.R;
@@ -35,14 +35,12 @@ import it.unitn.disi.fumiprovv.roommates.viewmodels.JoinHouseViewModel;
  * create an instance of this fragment.
  */
 public class HouseCreationFragment extends Fragment {
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -79,17 +77,16 @@ public class HouseCreationFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         MainActivity mainActivity = (MainActivity) requireActivity();
         mainActivity.setDrawerLocked(true);
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_house_creation, container, false);
-        Button joinBtn = (Button) view.findViewById(R.id.joinButton);
+        Button joinBtn = view.findViewById(R.id.joinButton);
         joinBtn.setOnClickListener((a) -> onJoinButtonClick(view));
-        Button createBtn = (Button) view.findViewById(R.id.createButton);
+        Button createBtn = view.findViewById(R.id.createButton);
         createBtn.setOnClickListener((a) -> onCreateButtonClick(view));
         JoinHouseViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(JoinHouseViewModel.class);
         if (sharedViewModel.isHouseIdValid()) {
@@ -105,22 +102,27 @@ public class HouseCreationFragment extends Fragment {
 
         String houseId = ((TextView) view.findViewById(R.id.joinHouseField)).getText().toString();
         String userId = mAuth.getUid();
-        db.collection("case").document(houseId).update("roommates", FieldValue.arrayUnion(new Roommate(userId, Timestamp.now(), false)))
-                .addOnSuccessListener(task -> {
-                    //House house = task.toObject(House.class);
-                    //house.addRoommate(new Roommate(userId, Timestamp.now(), false));
-                    //db.collection("case").document(houseId).set(house);
-                    db.collection("utenti").document(userId).update("casa", houseId);
 
-                    updateHouseId(houseId);
+        if (userId == null) return;
+        if (houseId.isEmpty()) {
+            Toast.makeText(getContext(), R.string.error_empty_house_id, Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.GONE);
+            return;
+        }
 
-                    progressBar.setVisibility(View.GONE);
-                    NavigationUtils.navigateTo(R.id.action_houseCreationFragment_to_homeFragment, view);
-                })
-                .addOnFailureListener(task -> {
-                   Toast.makeText(getContext(), R.string.error_join_house, Toast.LENGTH_SHORT).show();
-                    progressBar.setVisibility(View.GONE);
-                });
+        WriteBatch batch = db.batch();
+        batch.update(db.collection("case").document(houseId), "roommates", FieldValue.arrayUnion(new Roommate(userId, Timestamp.now(), false)));
+        batch.update(db.collection("utenti").document(userId), "casa", houseId);
+
+        batch.commit().addOnSuccessListener(task -> {
+            updateHouseId(houseId);
+
+            progressBar.setVisibility(View.GONE);
+            NavigationUtils.navigateTo(R.id.action_houseCreationFragment_to_homeFragment, view);
+        }).addOnFailureListener(task -> {
+            Toast.makeText(getContext(), R.string.error_join_house, Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.GONE);
+        });
     }
 
     public void updateHouseId(String houseId) {
@@ -140,22 +142,26 @@ public class HouseCreationFragment extends Fragment {
         String houseName = ((TextView) view.findViewById(R.id.createHouseField)).getText().toString();
         String userId = mAuth.getUid();
         House house = House.createHouse(houseName, userId);
-        db.collection("case").document(house.getId()).set(house).addOnSuccessListener(aVoid -> {
-                    db.collection("utenti").document(userId).update("casa", house.getId());
 
-                    Bundle bundle = new Bundle();
-                    bundle.putString("houseName", house.getName());
-                    bundle.putString("houseId", house.getId());
+        if (userId == null)
+            return;
 
-                    updateHouseId(house.getId());
+        WriteBatch batch = db.batch();
+        batch.set(db.collection("case").document(house.getId()), house);
+        batch.update(db.collection("utenti").document(userId), "casa", house.getId());
 
-                    progressBar.setVisibility(View.GONE);
-                    NavigationUtils.navigateTo(R.id.action_houseCreationFragment_to_houseCreatedFragment, view, bundle);
-                })
-                .addOnFailureListener(e -> {
-                    Log.d("HouseCreationFragment", "Error writing document", e);
-                    Toast.makeText(getContext(), "Error creating house", Toast.LENGTH_SHORT).show();
-                    progressBar.setVisibility(View.GONE);
-                });
+        batch.commit().addOnSuccessListener(task -> {
+            Bundle bundle = new Bundle();
+            bundle.putString("houseName", house.getName());
+            bundle.putString("houseId", house.getId());
+
+            updateHouseId(house.getId());
+
+            progressBar.setVisibility(View.GONE);
+            NavigationUtils.navigateTo(R.id.action_houseCreationFragment_to_houseCreatedFragment, view, bundle);
+        }).addOnFailureListener(task -> {
+            Toast.makeText(getContext(), R.string.error_create_house, Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.GONE);
+        });
     }
 }
