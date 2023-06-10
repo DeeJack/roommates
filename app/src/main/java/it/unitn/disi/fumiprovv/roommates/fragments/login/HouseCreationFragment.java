a92eb5f4d0174a6532776ffc79ae483f14e66140
Lorenzo <lorenzofumi@gmail.com>
2023-06-10 17:38:30 +0200

    Using DB transaction (batch) to join/create a house.

--- app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/login/HouseCreationFragment.java
+++ app/src/main/java/it/unitn/disi/fumiprovv/roommates/fragments/login/HouseCreationFragment.java
@@ -3,7 +3,6 @@ package it.unitn.disi.fumiprovv.roommates.fragments.login;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
